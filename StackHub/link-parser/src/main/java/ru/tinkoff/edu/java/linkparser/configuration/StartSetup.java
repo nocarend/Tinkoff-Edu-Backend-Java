689e3b70969b9e0864670a068d2052227c7fa837
nocarend <58514394+nocarend@users.noreply.github.com>
2023-03-18 05:49:04 +0300

    gitignore antlr generated classes

---
+++ StackHub/link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/configuration/StartSetup.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.edu.java.linkparser.configuration;
+
+import antlr.LinkParserParser;
+
+public class StartSetup {
+    private StartSetup() {
+        throw new AssertionError();
+    }
+
+    public static LinkParserParser.LinkContext get(LinkParserParser parser) {
+        return parser.link();
+    }
+}
