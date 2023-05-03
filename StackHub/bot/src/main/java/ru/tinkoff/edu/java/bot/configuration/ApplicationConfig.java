2a2f4939a2078a54fc577ef98a62c62e4cc83116
nocarend <gudvin0203@gmail.com>
2023-05-03 19:07:11 +0300

    Files changed

--- StackHub/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
+++ StackHub/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
@@ -2,6 +2,7 @@ package ru.tinkoff.edu.java.bot.configuration;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
